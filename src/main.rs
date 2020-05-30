a56fb38362262193ad5809818d26071610148f23
youyu07 <youyu07@163.com>
2020-05-30 17:55:36 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
+async fn run() {
+    let adapter = wgpu::Adapter::request(
+        &wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::Default,
+            compatible_surface: None,
+        },
+        wgpu::BackendBit::PRIMARY,
+    )
+    .await
+    .unwrap();
+
+    let (device, queue) = adapter
+        .request_device(&wgpu::DeviceDescriptor {
+            extensions: wgpu::Extensions {
+                anisotropic_filtering: false,
+            },
+            limits: wgpu::Limits::default(),
+        })
+        .await;
+
+    let texture_extent = wgpu::Extent3d {
+        width: 1920,
+        height: 1080,
+        depth: 1,
+    };
+
+    let texture = device.create_texture(&wgpu::TextureDescriptor {
+        size: texture_extent,
+        mip_level_count: 1,
+        array_layer_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D2,
+        format: wgpu::TextureFormat::Bgra8UnormSrgb,
+        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT | wgpu::TextureUsage::COPY_SRC,
+        label: None,
+    });
+
+    let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        size: (1920 * 1080) as u64 * std::mem::size_of::<u32>() as u64,
+        usage: wgpu::BufferUsage::MAP_READ | wgpu::BufferUsage::COPY_DST,
+        label: None,
+    });
+
+    let command_buffer = {
+        let mut encoder =
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                attachment: &texture.create_default_view(),
+                resolve_target: None,
+                load_op: wgpu::LoadOp::Clear,
+                store_op: wgpu::StoreOp::Store,
+                clear_color: wgpu::Color::RED,
+            }],
+            depth_stencil_attachment: None,
+        });
+
+        encoder.copy_texture_to_buffer(
+            wgpu::TextureCopyView {
+                texture: &texture,
+                mip_level: 0,
+                array_layer: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            wgpu::BufferCopyView {
+                buffer: &output_buffer,
+                offset: 0,
+                bytes_per_row: std::mem::size_of::<u32>() as u32 * 1920,
+                rows_per_image: 0,
+            },
+            texture_extent,
+        );
+
+        encoder.finish()
+    };
+
+    queue.submit(&[command_buffer]);
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    futures::executor::block_on(run());
 }
